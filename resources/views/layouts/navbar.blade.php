a8dd9a40ea3647f03e3d3d5330e5e4fe9ac08b44
Merdjan Tekfidan <merttekfidan@gmail.com>
2019-08-22 21:25:54 +0200

    Set new navbar links

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -7,12 +7,16 @@
     <div class="collapse navbar-collapse" id="navbarResponsive">
       <ul class="navbar-nav ml-auto">
         <li class="nav-item active">
-          <a class="nav-link" href="#">Home
+          <a class="nav-link" href="/">Home
             <span class="sr-only">(current)</span>
           </a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href={{route('product')}}>Product</a>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Product</a>
+          <div class="dropdown-menu" aria-labelledby="dropdown03">
+              <a class="dropdown-item" href="{{route('listProduct')}}">List Products</a>
+              <a class="dropdown-item" href="{{route('addProduct')}}">Add new product</a>
+            </div>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">Services</a>
