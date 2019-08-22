7b4a0f2d485777046d8777e37abccf3abb829689
Merdjan Tekfidan <merttekfidan@gmail.com>
2019-08-22 19:51:27 +0200

    Syntax correction

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(App\Product::class, 5)->create();
+        factory(App\Product::class, 5)->create();
     }
 }
