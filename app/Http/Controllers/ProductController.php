acc6ba0efca4cd1960679fbe9399fffc57a388de
Merdjan Tekfidan <merttekfidan@gmail.com>
2019-08-24 20:20:43 +0200

    Change request class name into related request in update paramater

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -74,7 +74,7 @@ class ProductController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(ProductRequest $request, $id)
     {
         $products = Product::findOrFail($id);
         $products->title = $request->title;
